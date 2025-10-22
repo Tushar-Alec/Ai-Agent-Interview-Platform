894fe81b9ef426915ff12e1ceeafbd61962c0276
Tushar-Alec <petermoor822@gmail.com>
2025-10-22 22:29:26 +0530

    Enhance layout and page structure with navigation, interview sections, and new InterviewCard component. update AuthForm branding and add dayjs dependency

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -63,7 +63,7 @@ const isSignIn = type === "sign-in";
             <div className="flex flex-col gap-6 card py-14 px-10">
                 <div className="flex flex-row gap-2 justify-center">
                     <img src="/logo.svg" alt="Logo" width={38} height={32} />
-                    <h2 className="text-primary-100">InterUwU</h2>
+                    <h2 className="text-primary-100">InterU</h2>
                     
                 </div>
                 <h3 >Practice job interview with AI</h3>
