0f04cf74131d5cf2c6d4f34db696187d93d3dea8
Tushar-Alec <petermoor822@gmail.com>
2025-11-05 21:51:04 +0530

    Enhance authentication flow by implementing user session management and redirect logic in layout components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,14 @@
 import { ReactNode } from "react";
 import Link from "next/link";
+import { is } from "zod/locales";
+import { isAuthenticated } from "@/lib/actions/auth.action";
+import { redirect } from "next//navigation";
 
-const RootLayout = ({children}: {children: ReactNode }) => {
+const RootLayout = async({children}: {children: ReactNode }) => {
+    const isUserAuthenticated = await isAuthenticated();
+
+    if (!isUserAuthenticated) redirect('/sign-in');
+    
     return (
         <div className="root-layout">
             <nav>
